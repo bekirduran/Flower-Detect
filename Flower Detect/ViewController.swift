d0c42e4742e71e950bb2111d2ee5c5fdb0be2dab
Bekir Duran <akinmihal@gmail.com>
2020-04-22 02:38:19 +0300

    Initial Commit

---
+++ Flower Detect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flower Detect
+//
+//  Created by Bekir Duran on 22.04.2020.
+//  Copyright © 2020 info. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
